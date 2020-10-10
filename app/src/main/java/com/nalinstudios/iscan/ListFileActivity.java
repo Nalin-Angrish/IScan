e769c27d2b16017a84a2eb9169c783277d7c4d6e
Nalin Angrish <nalinangrish2005@gmail.com>
2020-10-10 20:08:12 +0530

    Added option for sharing files in explorer view by long pressing

--- app/src/main/java/com/nalinstudios/iscan/ListFileActivity.java
+++ app/src/main/java/com/nalinstudios/iscan/ListFileActivity.java
@@ -1,13 +1,16 @@
 package com.nalinstudios.iscan;
 
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.FileProvider;
 
 import java.io.File;
@@ -61,6 +64,8 @@ public class ListFileActivity extends ListActivity {
         // Put the data into the list
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_2, android.R.id.text1, values);
         setListAdapter(adapter);
+
+        getListView().setOnItemLongClickListener(new LongPress());
     }
 
 
@@ -89,6 +94,52 @@ public class ListFileActivity extends ListActivity {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             startActivity(Intent.createChooser(intent, "Open Using"));
+        }
+    }
+
+
+    /**
+     * A class to listen for long press events.
+     * This would be used to provide the user the options to share or view the pdf
+     */
+    private class LongPress implements AdapterView.OnItemLongClickListener {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            final String filename = (String) getListAdapter().getItem(position);
+            new AlertDialog.Builder(ListFileActivity.this)
+                    .setIcon(android.R.drawable.btn_star)
+                    .setMessage("What Operation do you want to do with "+ new File(filename).getName())
+                    .setNeutralButton("Nothing", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .setNegativeButton("View", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setDataAndType(FileProvider.getUriForFile(ListFileActivity.this, getPackageName()+ ".provider", new File(path,filename)), "application/pdf");
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                            startActivity(Intent.createChooser(intent, "Open Using"));
+                        }
+                    })
+                    .setPositiveButton("Share", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            Intent intent = new Intent(Intent.ACTION_SEND);
+                            intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(ListFileActivity.this, getPackageName()+ ".provider", new File(path,filename)));
+                            intent.setType("application/pdf");
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                            startActivity(Intent.createChooser(intent, "Share Using"));
+                        }
+                    })
+                    .show();
+            return true;
         }
     }
 }
